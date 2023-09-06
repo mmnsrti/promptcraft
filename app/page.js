bdefceae549079e05aafe06f55316d6ab20d2671
mohamad mahdi nosrati <mmnsrti@gmail.com>
2023-09-06 21:44:34 +0330

    change some titles and hero section description

--- app/page.js
+++ app/page.js
@@ -1,21 +1,19 @@
-import Feed from '@components/Feed'
-
+import Feed from "@components/Feed";
 
 export default function Home() {
+  const TITLE = "Welcome to";
+  const DESCRIPTION =
+    "Welcome to PromptCraft – your creative AI prompt platform! PromptCraft allows you to effortlessly create, share, like, and use prompts for AI projects. Whether you're an artist, writer, or AI enthusiast, our platform is your gateway to unleashing the power of creative AI.";
+
   return (
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
-        Discover
+        {TITLE}
         <br className="max-lg:hidden" />
-        <span className="orange_gradient text-center">Ai Powered Prompt</span>
+        <span className="orange_gradient text-center">Prompt Craft</span>
       </h1>
-      <p className="desc text-center">
-        Ai Powered Prompt is a prompt generator powered by AI. It is a
-        collaborative project to build a prompt generator that can be used by
-        anyone.
-      </p>
-      <Feed/>
-
+      <p className="desc text-center">{DESCRIPTION}</p>
+      <Feed />
     </section>
   );
 }
